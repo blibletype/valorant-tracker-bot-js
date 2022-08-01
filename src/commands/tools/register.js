c1ef5cb37af9c2f96ff54d7c99568a4a09a3d3ed
blibletype <maxymkoval2510@gmail.com>
2022-08-01 20:57:57 +0300

    register fix

--- src/commands/tools/register.js
+++ src/commands/tools/register.js
@@ -24,6 +24,22 @@ module.exports = {
 
         const name = interaction.options.getString('name');
         const tag = interaction.options.getString('tag');
+        const discord_id = interaction.user.id;
+
+        const user = await User.findOne({ where: { discord_id: discord_id } });
+            
+        if (!(user === null)) {
+            embed = new EmbedBuilder()
+                .setTitle(`Ви уже зареєстровані 🚫`)
+                .setDescription(`Якщо це не так зв'яжіться з адміністрацією`)
+                .setColor('#f80c0c')
+                .setTimestamp(Date.now());
+
+            await interaction.editReply({
+                embeds: [embed],
+            });
+            return;
+        }      
         
         try {
             const res = await axios.get(`https://api.henrikdev.xyz/valorant/v1/account/${name}/${tag}`, {
@@ -32,25 +48,25 @@ module.exports = {
                 }
             });
             
-            const discord_id = interaction.user.id;
+            
             const account_level = res.data.data.account_level;
             const puuid = res.data.data.puuid;
             const region = res.data.data.region;
             const small_card_url = res.data.data.card.small;
             const large_card_url = res.data.data.card.large;
             const wide_card_url = res.data.data.card.wide;
-            const elo = res.data.data.elo;
-            const current_tier_patched = res.data.data.currenttierpatched;
 
-            const user = await User.findOne({ where: { discord_id: discord_id } });
             
-            if (user === null) {
                 try {
                     const res = await axios.get(`https://api.henrikdev.xyz/valorant/v1/by-puuid/mmr/${region}/${puuid}`, {
                     headers: {
                         'Authorization': `${process.env.api_key}`
                     }
                 });
+
+                const elo = res.data.data.elo;
+                const current_tier_patched = res.data.data.currenttierpatched;
+
                 await User.create({
                     discord_id: discord_id,
                     valorant_name: name,
@@ -87,18 +103,6 @@ module.exports = {
                         embeds: [embed],
                     });
                 }
-            
-            } else {
-                embed = new EmbedBuilder()
-                    .setTitle(`Ви уже зареєстровані 🚫`)
-                    .setDescription(`Якщо це не так зв'яжіться з адміністрацією`)
-                    .setColor('#f80c0c')
-                    .setTimestamp(Date.now());
-
-                await interaction.editReply({
-                    embeds: [embed],
-                });
-            }      
         } catch (error) {
             const err = error.response.data.status;
             let title, description;
