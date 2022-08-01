f8f3dd5e3aee1f05742681fe8c3c98142d460d9b
blibletype <maxymkoval2510@gmail.com>
2022-08-01 10:50:53 +0300

    add ping command and interactionCreate event

--- src/bot.js
+++ src/bot.js
@@ -6,6 +6,7 @@ const fs = require('fs');
 
 const client = new Client({ intents: GatewayIntentBits.Guilds });
 client.commands = new Collection();
+client.commandArray = [];
 
 const functionFolders = fs.readdirSync('./src/functions');
 for (const folder of functionFolders) {
