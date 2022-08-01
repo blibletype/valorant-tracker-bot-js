9d1a5dc757a1d68d6bc6d2f889699827d21ee3d9
blibletype <maxymkoval2510@gmail.com>
2022-08-01 10:17:57 +0300

    add handlers

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,20 @@
 require('dotenv').config();
 
 const { token } = process.env;
-const { Client, Collection } = require('discord.js');
+const { Client, Collection, GatewayIntentBits } = require('discord.js');
 const fs = require('fs');
+
+const client = new Client({ intents: GatewayIntentBits.Guilds });
+client.commands = new Collection();
+
+const functionFolders = fs.readdirSync('./src/functions');
+for (const folder of functionFolders) {
+    const functionFiles = fs
+        .readdirSync(`./src/functions/${folder}`)
+        .filter(file => file.endsWith('.js'));
+    for (const file of functionFiles) 
+        require(`./functions/${folder}/${file}`)(client);
+} 
+client.handleEvents();
+client.handleCommands();
+client.login(token)  
